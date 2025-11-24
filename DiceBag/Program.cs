9ec0cb0b4383eef4e62e3dfac899329b27112f46
Victoria Alaniz <tori@alaniz.tech>
2025-11-23 21:06:15 -0600

    fixed some labeling

--- DiceBag/Program.cs
+++ DiceBag/Program.cs
@@ -1,30 +1,31 @@
 ﻿using System;
 using System.Diagnostics;
 
-namespace DiceBag {
-    class Program {
-        static void Main(string[] args) {
-            Random dice = new Random();
-            var scores = new List<int>();
+namespace DiceBag;
+class Program {
+    static void Main(string[] args) {
+        Random dice = new Random();
+        var scores = new List<int>();
 
-            for (var x = 0; x < 6; x++) {
-                var rolls = new List<int>();
-                
-                // roll 4d6
-                for (int i = 0; i < 4; i++) {
-                    rolls.Add(dice.Next(1, 7));
-                }
-
-                // remove minimum
-                rolls.Remove(rolls.Min());
-                
-                // sum remaining 3 values
-                scores.Add(rolls.Sum());
+        for (var i = 0; i < 6; i++) {
+            var rolls = new List<int>();
+            
+            // roll 4d6
+            for (int j = 0; j < 4; j++) {
+                rolls.Add(dice.Next(1, 7));
             }
 
-            foreach (var score in scores) {
-                Console.WriteLine($"Your score is {score}");
-            }
+            // remove minimum
+            rolls.Remove(rolls.Min());
+            
+            // sum remaining 3 values
+            scores.Add(rolls.Sum());
+        }
+
+        foreach (var score in scores) {
+            Console.WriteLine($"Your score is {score}");
         }
     }
+    
+    
 }
